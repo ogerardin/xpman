7073f626657971aa5c2bd372f7bef0f5c968b18d
ogerardin@yahoo.com <bebe081060>
2020-06-18 13:57:38 +0200

    Fixed ZiboMod version reading

--- xpman-api/src/main/java/com/ogerardin/xplane/config/aircrafts/custom/ZiboMod738.java
+++ xpman-api/src/main/java/com/ogerardin/xplane/config/aircrafts/custom/ZiboMod738.java
@@ -8,16 +8,20 @@ import com.ogerardin.xplane.util.GoogleDriveClient;
 import com.ogerardin.xplane.util.Maps;
 import lombok.Getter;
 import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
 import java.net.URL;
 import java.security.GeneralSecurityException;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
+import java.util.OptionalInt;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 @SuppressWarnings("unused")
+@Slf4j
 public class ZiboMod738 extends Aircraft implements CustomAircraft {
 
     /** The Google Drive folder ID of the folder containing published updates */
@@ -53,18 +57,36 @@ public class ZiboMod738 extends Aircraft implements CustomAircraft {
     public String getLatestVersion() throws GeneralSecurityException, IOException {
         // The aircraft is published on a Google drive: https://drive.google.com/drive/folders/0B-tdl3VvPeOOYm12Wm80V04wdDQ
         // Full versions are published as a file B737-800X_<version>_full.zip, e.g. B737-800X_3_42_full.zip
-        // Patches are published as invremental files B737-800X_<version>_<patch>.zip, e.g. B737-800X_3_42_10.zip
+        // Patches are published as incremental files B738X_<version>_<patch>.zip, e.g. B737-800X_3_42_10.zip
+
+        // obtain list of files on the Google drive
         GoogleDriveClient client = new GoogleDriveClient();
         List<File> files = client.getFiles(ZIBO_FOLDER_ID);
+
+        // try to find file matching full version pattern and extract version
         Pattern pattern = Pattern.compile("B737-800X_([\\d_]+)_full.zip");
-        for (File file : files) {
-            Matcher matcher = pattern.matcher(file.getName());
-            if (matcher.matches()) {
-                String g1 = matcher.group(1);
-                String version = g1.replaceAll("_", ".");
-                return version;
-            }
+        Optional<String> maybeVersion = files.stream()
+                .map(File::getName)
+                .map(pattern::matcher)
+                .filter(Matcher::matches)
+                .findAny()
+                .map(matcher -> matcher.group(1));
+        if (! maybeVersion.isPresent()) {
+            return null;
         }
-        return null;
+
+        // try to find patches for this version, keep the max
+        String version = maybeVersion.get();
+        Pattern pattern2 = Pattern.compile("B738X_" + version + "_(\\d+).zip");
+        OptionalInt maybePatch = files.stream()
+                .map(File::getName)
+                .map(pattern2::matcher)
+                .filter(Matcher::matches)
+                .map(matcher -> matcher.group(1))
+                .mapToInt(Integer::valueOf)
+                .max();
+
+        version = version.replaceAll("_", ".");
+        return maybePatch.isPresent() ? String.format("%s.%d", version, maybePatch.getAsInt()) : version;
     }
 }
