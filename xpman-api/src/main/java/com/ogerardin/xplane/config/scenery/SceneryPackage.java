ecd88c441f3eaa6e35579afa085d70cd9b24de72
ogerardin@yahoo.com <bebe081060>
2020-07-11 02:36:33 +0200

    Added tile count in scenery.

--- xpman-api/src/main/java/com/ogerardin/xplane/config/scenery/SceneryPackage.java
+++ xpman-api/src/main/java/com/ogerardin/xplane/config/scenery/SceneryPackage.java
@@ -1,10 +1,14 @@
 package com.ogerardin.xplane.config.scenery;
 
+import com.ogerardin.xplane.util.FileUtils;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NonNull;
+import lombok.SneakyThrows;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.List;
 
 @Data
 public class SceneryPackage {
@@ -14,12 +18,21 @@ public class SceneryPackage {
     @NonNull
     final Path folder;
 
+    @Getter(lazy = true)
+    private final int tileCount = countTiles();
+
+    @SneakyThrows
+    private int countTiles() {
+        return FileUtils.findFiles(getEarthNavDataFolder(), path -> path.getFileName().toString().endsWith(".dsf")).size();
+    }
+
     private Integer rank = null;
 
     public String getName() {
         return folder.getFileName().toString();
     }
 
+    @SuppressWarnings("unused")
     public boolean isAirport() {
         return Files.exists(getEarthNavDataFolder().resolve("apt.dat"));
     }
@@ -28,6 +41,7 @@ public class SceneryPackage {
         return folder.resolve(EARTH_NAV_DATA);
     }
 
+    @SuppressWarnings("unused")
     public boolean isLibrary() {
         return Files.exists(folder.resolve("library.txt"));
     }
@@ -35,6 +49,7 @@ public class SceneryPackage {
     public String getVersion() {
         return null;
     }
+
 
 
 }
