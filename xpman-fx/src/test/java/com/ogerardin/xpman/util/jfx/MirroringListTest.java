e410d480ac240e424d23063aedff1ad8a50871ed
Olivier Gérardin <ogerardin@yahoo.com>
2021-03-08 23:30:26 +0100

    Observability experiments

---
+++ xpman-fx/src/test/java/com/ogerardin/xpman/util/jfx/MirroringListTest.java
@@ -0,0 +1,48 @@
+package com.ogerardin.xpman.util.jfx;
+
+import com.google.api.client.googleapis.notifications.NotificationUtils;
+import com.sun.javafx.collections.ObservableListWrapper;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@Slf4j
+class MirroringListTest {
+
+    @Test
+    public void mirrors() {
+
+        final ObservableListWrapper<String> observableList = new ObservableListWrapper<>(new ArrayList<>());
+        observableList.add(UUID.randomUUID().toString());
+
+        final MirroringList<String, String> mirroringList = new MirroringList<>(observableList, MirroringListTest::transform);
+        log.debug("source list: {}", observableList);
+        log.debug("target list: {}", mirroringList);
+
+        assertEquals(expected(observableList), mirroringList);
+
+        observableList.add(UUID.randomUUID().toString());
+        log.debug("source list: {}", observableList);
+        log.debug("target list: {}", mirroringList);
+
+        assertEquals(expected(observableList), mirroringList);
+    }
+
+    private static String transform(String s) {
+        return new StringBuilder(s).reverse().toString();
+    }
+
+    private List<String> expected(ObservableListWrapper<String> observableList) {
+        final List<String> expected = observableList.stream()
+                .map(MirroringListTest::transform)
+                .collect(Collectors.toList());
+        return expected;
+    }
+
+}
