141e4c34dc82b91def3f22e4d66c40fb7602c62d
Olivier Gérardin <ogerardin@yahoo.com>
2021-04-03 02:20:02 +0200

    Introduced TableRowFactory.

--- xpman-fx/src/main/java/com/ogerardin/xpman/util/jfx/CellUpdater.java
+++
@@ -1,5 +0,0 @@
-package com.ogerardin.xpman.util.jfx;
-
-public interface CellUpdater<T> {
-    void updateItem(T value, boolean empty);
-}
--- xpman-fx/src/main/java/com/ogerardin/xpman/util/jfx/TreeTableCellFactory.java
+++ xpman-fx/src/main/java/com/ogerardin/xpman/util/jfx/TreeTableCellFactory.java
@@ -4,5 +4,8 @@ import javafx.scene.control.TreeTableCell;
 import javafx.scene.control.TreeTableColumn;
 import javafx.util.Callback;
 
+/**
+ * Interface expected by {@link TreeTableColumn#setCellFactory}
+ */
 public interface TreeTableCellFactory<S, T> extends Callback<TreeTableColumn<S, T>, TreeTableCell<S, T>> {
 }
