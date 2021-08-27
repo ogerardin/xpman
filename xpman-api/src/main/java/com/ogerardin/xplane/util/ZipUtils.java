42be6298df3857a87f1e6d3afb774e6402841e53
Olivier Gérardin <ogerardin@yahoo.com>
2021-08-28 00:00:00 +0200

    Fixed: unzip failed when intermediate directories were missing New method: getAsText

--- xpman-api/src/main/java/com/ogerardin/xplane/util/ZipUtils.java
+++ xpman-api/src/main/java/com/ogerardin/xplane/util/ZipUtils.java
@@ -1,10 +1,10 @@
 package com.ogerardin.xplane.util;
 
+import com.google.common.io.CharStreams;
 import lombok.experimental.UtilityClass;
 import lombok.extern.slf4j.Slf4j;
 
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -37,10 +37,11 @@ public class ZipUtils {
                 if (! target.startsWith(targetFolder)) {
                     throw new IOException("Entry outside of target directory: " + target);
                 }
-                if (zipEntry.isDirectory()) {
-                    Files.createDirectories(target);
-                } else {
+                // we ignore directory entries; any non-existing intermediate directories will be created before
+                // extracting the file
+                if (! zipEntry.isDirectory()) {
                     log.debug("Extracting {} as {}", entryName, target);
+                    Files.createDirectories(target.getParent());
                     InputStream inputStream = zip.getInputStream(zipEntry);
                     Files.copy(inputStream, target, StandardCopyOption.REPLACE_EXISTING);
                 }
@@ -56,7 +57,27 @@ public class ZipUtils {
         }
     }
 
+    public static String getAsText(Path zipFile, Path path) throws IOException {
+        try (ZipFile zip = new ZipFile(zipFile.toFile(), Charset.forName("CP437"))) {
+            Enumeration<? extends ZipEntry> entries = zip.entries();
+            while (entries.hasMoreElements()) {
+                ZipEntry zipEntry = entries.nextElement();
+                String entryName = zipEntry.getName();
+                if (! Paths.get(entryName).equals(path)) {
+                    continue;
+                }
+                InputStream inputStream = zip.getInputStream(zipEntry);
+                try (Reader reader = new InputStreamReader(inputStream)) {
+                    return CharStreams.toString(reader);
+                }
+            }
+        }
+        throw new FileNotFoundException(path.toString());
+    }
+
     public interface ProgressListener {
         void extracting(String filename);
     }
+
+
 }
