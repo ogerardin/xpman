1688f3c825ccfec1c3c48b0f1a063ff726c75e62
Olivier Gérardin <ogerardin@yahoo.com>
2021-09-10 00:56:49 +0200

    Added animation to breakdown chart when loading

--- xpman-fx/src/main/java/com/ogerardin/xpman/panels/xplane/breakdown/SegmentView.java
+++ xpman-fx/src/main/java/com/ogerardin/xpman/panels/xplane/breakdown/SegmentView.java
@@ -1,16 +1,27 @@
 package com.ogerardin.xpman.panels.xplane.breakdown;
 
+import javafx.beans.binding.Bindings;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 import javafx.scene.control.OverrunStyle;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
 
 public class SegmentView extends StackPane {
 
+    private final ImageView DOTS = new ImageView(new Image(SegmentView.class.getResource("/dots.gif").toExternalForm()));
+
     public SegmentView(Segment segment) {
         Label label = new Label();
         label.textProperty().bind(segment.textProperty());
+        label.graphicProperty().bind(Bindings
+                .when(segment.computingProperty())
+                        .then(DOTS)
+                        .otherwise((ImageView) null)
+                );
+//        label.setGraphic(DOTS);
         label.setStyle("-fx-font-weight: bold; -fx-text-fill: white; -fx-font-size: 1.2em;");
         label.setTextOverrun(OverrunStyle.ELLIPSIS);
         setAlignment(label, Pos.CENTER_LEFT);
