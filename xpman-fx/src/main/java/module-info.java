63dec9bd3f344f5e0ee918f4724899e83dbe4184
Olivier Gérardin <ogerardin@yahoo.com>
2022-02-06 22:13:52 +0100

    Cleanup

--- xpman-fx/src/main/java/module-info.java
+++ xpman-fx/src/main/java/module-info.java
@@ -15,10 +15,11 @@ module xpman.fx {
 
     opens com.ogerardin.xpman.panels.about to javafx.base, javafx.fxml;
     opens com.ogerardin.xpman.panels.navdata to javafx.base, javafx.fxml, spring.expression;
-    opens com.ogerardin.xpman.panels.plugins to  javafx.base, javafx.fxml, spring.expression;
-    opens com.ogerardin.xpman.panels.scenery to  javafx.base, javafx.fxml, spring.expression;
-    opens com.ogerardin.xpman.panels.scenery.rules to  javafx.base, javafx.fxml, spring.expression;
-    opens com.ogerardin.xpman.panels.aircrafts to  javafx.base, javafx.fxml, spring.expression;
+    opens com.ogerardin.xpman.panels.plugins to javafx.base, javafx.fxml, spring.expression;
+    opens com.ogerardin.xpman.panels.scenery to javafx.base, javafx.fxml, spring.expression;
+    opens com.ogerardin.xpman.panels.scenery.rules to javafx.base, javafx.fxml, spring.expression;
+    opens com.ogerardin.xpman.panels.aircrafts to javafx.base, javafx.fxml, spring.expression;
+    opens com.ogerardin.xpman.panels.aircrafts.details to javafx.base, javafx.fxml, spring.expression;
     opens com.ogerardin.xpman.scenery_organizer to spring.expression;
     opens com.ogerardin.xpman.panels.xplane to javafx.base, javafx.fxml;
     opens com.ogerardin.xpman.util.jfx to javafx.base, javafx.fxml;
