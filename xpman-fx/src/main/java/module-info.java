c13cc117514c1e649e2421a922359f1531e3a96e
Olivier Gérardin <ogerardin@yahoo.com>
2022-02-22 23:59:58 +0100

    Initiate java17 branch

--- xpman-fx/src/main/java/module-info.java
+++ xpman-fx/src/main/java/module-info.java
@@ -13,21 +13,22 @@ module xpman.fx {
     requires com.sun.jna;
     requires com.sun.jna.platform;
 
+    opens com.ogerardin.xpman to javafx.base, javafx.fxml;
+    opens com.ogerardin.xpman.config to com.google.gson;
+    opens com.ogerardin.xpman.diag to javafx.base, javafx.fxml;
+    opens com.ogerardin.xpman.install.wizard to javafx.base, javafx.fxml;
     opens com.ogerardin.xpman.panels.about to javafx.base, javafx.fxml;
+    opens com.ogerardin.xpman.panels.aircrafts to javafx.base, javafx.fxml, spring.expression;
+    opens com.ogerardin.xpman.panels.aircrafts.details to javafx.base, javafx.fxml, spring.expression;
     opens com.ogerardin.xpman.panels.navdata to javafx.base, javafx.fxml, spring.expression;
     opens com.ogerardin.xpman.panels.plugins to javafx.base, javafx.fxml, spring.expression;
     opens com.ogerardin.xpman.panels.scenery to javafx.base, javafx.fxml, spring.expression;
     opens com.ogerardin.xpman.panels.scenery.rules to javafx.base, javafx.fxml, spring.expression;
-    opens com.ogerardin.xpman.panels.aircrafts to javafx.base, javafx.fxml, spring.expression;
-    opens com.ogerardin.xpman.panels.aircrafts.details to javafx.base, javafx.fxml, spring.expression;
     opens com.ogerardin.xpman.panels.scenery.wizard to javafx.base, javafx.fxml, spring.expression;
-    opens com.ogerardin.xpman.scenery_organizer to javafx.base, javafx.fxml, spring.expression;
     opens com.ogerardin.xpman.panels.xplane to javafx.base, javafx.fxml;
-    opens com.ogerardin.xpman.util.jfx to javafx.base, javafx.fxml;
+    opens com.ogerardin.xpman.scenery_organizer to javafx.base, javafx.fxml, spring.expression, com.google.gson;
+    opens com.ogerardin.xpman.util.jfx to javafx.base, javafx.fxml, com.google.gson;
     opens com.ogerardin.xpman.util.jfx.wizard to javafx.base, javafx.fxml;
-    opens com.ogerardin.xpman.install.wizard to javafx.base, javafx.fxml;
-    opens com.ogerardin.xpman.diag to javafx.base, javafx.fxml;
-    opens com.ogerardin.xpman to javafx.base, javafx.fxml;
 
     exports com.ogerardin.xpman;
 }
