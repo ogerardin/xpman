f91e02920979b4c36ea052c91b87df9cf8d1e1cd
Olivier Gérardin <ogerardin@yahoo.com>
2022-03-06 01:30:12 +0100

    Plugins: improvements

--- xpman-api/src/main/java/com/ogerardin/xplane/plugins/custom/AviTab.java
+++ xpman-api/src/main/java/com/ogerardin/xplane/plugins/custom/AviTab.java
@@ -1,6 +1,7 @@
 package com.ogerardin.xplane.plugins.custom;
 
-import com.ogerardin.xplane.plugins.Plugin;
+import com.ogerardin.xplane.XPlane;
+import com.ogerardin.xplane.plugins.XPlaneOrgPlugin;
 import com.ogerardin.xplane.util.IntrospectionHelper;
 import com.ogerardin.xplane.util.Maps;
 import lombok.SneakyThrows;
@@ -12,24 +13,31 @@ import java.util.Map;
 
 @SuppressWarnings("unused")
 @Slf4j
-public class AviTab extends Plugin {
+public class AviTab extends XPlaneOrgPlugin {
 
-    private static final String XPLANE_FORUM_URL = "https://forums.x-plane.org/index.php?/files/file/44825-avitab-vr-compatible-tablet-with-pdf-viewer-moving-maps-and-more/";
+    private static final String XPLANEORG_URL = "https://forums.x-plane.org/index.php?/files/file/44825-avitab-vr-compatible-tablet-with-pdf-viewer-moving-maps-and-more/";
 
     //TODO ability to manage charts
 
-    public AviTab(Path xplFile) throws InstantiationException {
-        super(xplFile, "AviTab: VR-compatible tablet with PDF viewer, moving maps and more");
-        IntrospectionHelper.require(xplFile.equals("AviTab.xpl"));
+    public AviTab(XPlane xPlane, Path xplFile) throws InstantiationException {
+        super(xPlane, xplFile, "AviTab", "VR-compatible tablet with PDF viewer, moving maps and more",
+                XPLANEORG_URL);
+        IntrospectionHelper.require(isAviTab(xplFile));
+    }
+
+    private boolean isAviTab(Path xplFile) {
+        return xplFile.getFileName().toString().equals("AviTab.xpl");
     }
 
     @SneakyThrows
     @Override
     public Map<String, URL> getLinks() {
-        return Maps.mapOf(
-                "X-Plane forum", new URL(XPLANE_FORUM_URL),
-                "GitHub page", new URL("https://github.com/fpw/avitab"),
-                "Wiki", new URL("https://github.com/fpw/avitab/wiki")
+        return  Maps.merge(
+                super.getLinks(),
+                Maps.mapOf(
+                        "GitHub page", new URL("https://github.com/fpw/avitab"),
+                        "Wiki", new URL("https://github.com/fpw/avitab/wiki")
+                )
         );
     }
 
