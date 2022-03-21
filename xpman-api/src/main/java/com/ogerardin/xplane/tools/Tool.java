b8b86dbcb8023204e72a2333db75d48560ad09c3
Olivier Gérardin <ogerardin@yahoo.com>
2022-03-21 10:09:04 +0100

    Tool management: wip

--- xpman-api/src/main/java/com/ogerardin/xplane/tools/Tool.java
+++ xpman-api/src/main/java/com/ogerardin/xplane/tools/Tool.java
@@ -3,32 +3,67 @@ package com.ogerardin.xplane.tools;
 import com.ogerardin.xplane.XPlane;
 import com.ogerardin.xplane.inspection.Inspections;
 import com.ogerardin.xplane.inspection.InspectionsProvider;
-import com.ogerardin.xplane.util.platform.Platforms;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
+import lombok.NonNull;
 import org.apache.commons.io.FilenameUtils;
 
 import java.nio.file.Path;
+import java.util.Optional;
 
 @Data
 @AllArgsConstructor
 public class Tool implements InspectionsProvider<Tool> {
 
-    private final Path path;
-
+    private final Path executable;
     private final String name;
+    private final ToolManifest manifest;
 
-    public Tool(Path path) {
-        this.path = path;
-        this.name = FilenameUtils.removeExtension(path.getFileName().toString());
+    @Getter(lazy = true)
+    private final String version = loadVersion();
+
+    /** Constructs an installed tool without known manifest */
+    public Tool(@NonNull Path path) {
+        this(path, FilenameUtils.removeExtension(path.getFileName().toString()), null);
     }
 
-    void run() {
-        Platforms.getCurrent().startApp(path);
+    /** Constructs an installed tool with specified manifest */
+    public Tool(@NonNull Path path, @NonNull ToolManifest manifest) {
+        this(path, manifest.getName(), manifest);
     }
+
+    /** Constructs a non-installed (available) tool */
+    public Tool(ToolManifest manifest) {
+        this(null, manifest.getName(), manifest);
+    }
+
 
     @Override
     public Inspections<Tool> getInspections(XPlane xPlane) {
         return null;
     }
+
+    public boolean isInstallable() {
+        return (executable == null) && (manifest != null);
+    }
+
+    public boolean isInstalled() {
+        return (executable != null) && (manifest != null);
+    }
+
+    public boolean isRunnable() {
+        return (executable != null);
+    }
+
+    public String loadVersion() {
+        if (executable == null) {
+            return null;
+        }
+        return Optional.ofNullable(manifest)
+                .map(ToolManifest::getVersionGetter)
+                .map(versionGetter -> versionGetter.apply(executable))
+                .orElse(null);
+    }
+
 }
---
+++ xpman-api/src/main/java/com/ogerardin/xplane/tools/ToolUtils.java
@@ -0,0 +1,15 @@
+package com.ogerardin.xplane.tools;
+
+import com.ogerardin.xplane.XPlane;
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class ToolUtils {
+
+    public static void installFromDmg(XPlane xPlane, String url) {
+    }
+
+    public static void installFromZip(XPlane xPlane, String url) {
+
+    }
+}
