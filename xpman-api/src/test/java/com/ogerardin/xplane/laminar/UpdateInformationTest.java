e7eb690c41c1a9cdc23fe7d875d501fb95247e90
Olivier Gérardin <ogerardin@yahoo.com>
2023-01-04 00:25:15 +0100

    Updated UpdateInformationTest

--- xpman-api/src/test/java/com/ogerardin/xplane/laminar/UpdateInformationTest.java
+++ xpman-api/src/test/java/com/ogerardin/xplane/laminar/UpdateInformationTest.java
@@ -1,24 +1,31 @@
 package com.ogerardin.xplane.laminar;
 
-import com.ogerardin.util.TimingExtension;
+import com.ogerardin.xplane.XPlaneMajorVersion;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @Slf4j
-@ExtendWith(TimingExtension.class)
+//@ExtendWith(TimingExtension.class)
 class UpdateInformationTest {
 
     @Test
     void testGetLatestVersions() {
-        String latestBeta = UpdateInformation.getLatestBeta();
-        log.info("latest beta: {}", latestBeta);
-        assertNotNull(latestBeta);
 
-        String latestRelease = UpdateInformation.getLatestFinal();
-        log.info("latest release: {}", latestRelease);
-        assertNotNull(latestRelease);
+        for (XPlaneMajorVersion majorVersion : XPlaneMajorVersion.values()) {
+            UpdateInformation updateInformation = new UpdateInformation(majorVersion);
+            log.info("major version: {}", majorVersion);
+
+            String latestBeta = updateInformation.getLatestBeta();
+            log.info("latest beta: {}", latestBeta);
+            assertNotNull(latestBeta);
+
+            String latestRelease = updateInformation.getLatestFinal();
+            log.info("latest release: {}", latestRelease);
+            assertNotNull(latestRelease);
+        }
+
+
     }
 }
