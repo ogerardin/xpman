5674cad9fc50f9531009f03026b2447817503e2c
Olivier Gérardin <ogerardin@yahoo.com>
2023-01-23 00:04:00 +0100

    New custom Dmax3D_EF2000

---
+++ xpman-api/src/main/java/com/ogerardin/xplane/aircrafts/custom/Dmax3D_EF2000.java
@@ -0,0 +1,43 @@
+package com.ogerardin.xplane.aircrafts.custom;
+
+import com.ogerardin.xplane.aircrafts.Aircraft;
+import com.ogerardin.xplane.file.AcfFile;
+import com.ogerardin.xplane.util.IntrospectionHelper;
+import com.ogerardin.xplane.util.Maps;
+import lombok.Getter;
+import lombok.SneakyThrows;
+
+import java.net.URL;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Dmax3D_EF2000 extends Aircraft {
+
+    private final Pattern FILE_PATTERN = Pattern.compile("ef2000_v(\\d)(\\d)");
+
+    @Getter(lazy = true)
+    private final String version = loadVersion();
+
+    @SneakyThrows
+    private String loadVersion() {
+        String folder = getAcfFile().getFile().getParent().getFileName().toString();
+        Matcher m = FILE_PATTERN.matcher(folder);
+        return m.matches() ? m.group(1) + "." + m.group(2) : null;
+    }
+
+    public Dmax3D_EF2000(AcfFile acfFile) throws InstantiationException {
+        super(acfFile, "Dmax3D Eurofighter Typhoon");
+        IntrospectionHelper.require(getStudio().equals("dmax3d.com") && getAcfName().equals("Eurofighter Typhoon"));
+    }
+
+    @SneakyThrows
+    public Map<String, URL> getLinks() {
+        return Maps.merge(super.getLinks(),
+                Maps.mapOf("Eurofighter Typhoon on dmax3d.com", new URL("http://www.dmax3d.com/dmax3d/eurofighter.html")
+                )
+        );
+    }
+
+
+}
