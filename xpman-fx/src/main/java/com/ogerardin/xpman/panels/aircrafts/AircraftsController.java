3d9c44978345051cb8b4e0a47b0853e481542a7a
Olivier Gérardin <ogerardin@yahoo.com>
2023-07-04 00:48:44 +0200

    Added aircraft filters (wip)

--- xpman-fx/src/main/java/com/ogerardin/xpman/panels/aircrafts/AircraftsController.java
+++ xpman-fx/src/main/java/com/ogerardin/xpman/panels/aircrafts/AircraftsController.java
@@ -8,14 +8,20 @@ import com.ogerardin.xpman.XPmanFX;
 import com.ogerardin.xpman.install.wizard.InstallWizard;
 import com.ogerardin.xpman.panels.Controller;
 import com.ogerardin.xpman.panels.ManagerItemsObservableList;
+import com.ogerardin.xpman.util.jfx.Filter;
 import com.ogerardin.xpman.util.jfx.menu.IntrospectingContextMenuTableRowFactory;
 import javafx.beans.binding.Bindings;
+import javafx.collections.transformation.FilteredList;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableView;
 import javafx.scene.control.ToolBar;
 import lombok.extern.slf4j.Slf4j;
+
+import java.util.Arrays;
+import java.util.function.Predicate;
 
 @Slf4j
 public class AircraftsController extends Controller {
@@ -25,12 +31,16 @@ public class AircraftsController extends Controller {
     private final XPlaneProperty xPlaneProperty;
 
     @FXML
+    private ComboBox<Filter<Aircraft>> filterCombo;
+
+    @FXML
     private ToolBar toolbar;
 
     @FXML
     private TableView<UiAircraft> aircraftsTable;
 
     private ManagerItemsObservableList<Aircraft, UiAircraft> uiItems;
+    private FilteredList<UiAircraft> filteredUiItems;
 
     public AircraftsController(XPmanFX mainController) {
         xPlaneProperty = mainController.xPlaneProperty();
@@ -38,12 +48,15 @@ public class AircraftsController extends Controller {
 
     @FXML
     public void initialize() {
+        initFilters(filterCombo);
+
         uiItems = new ManagerItemsObservableList<>(
                 this.xPlaneProperty,
                 XPlane::getAircraftManager,
                 aircraft -> new UiAircraft(aircraft, xPlaneProperty.get())
         );
-        aircraftsTable.setItems(uiItems);
+        filteredUiItems = new FilteredList<>(uiItems);
+        aircraftsTable.setItems(filteredUiItems);
 
         // add context menu
         aircraftsTable.setRowFactory(new IntrospectingContextMenuTableRowFactory<>(this));
@@ -59,6 +72,16 @@ public class AircraftsController extends Controller {
         );
     }
 
+    private void initFilters(ComboBox<Filter<Aircraft>> filterCombo) {
+        filterCombo.getItems().add(Filter.all());
+        filterCombo.getItems().addAll(
+                Arrays.stream(Aircraft.Category.values())
+                        .map(category -> new Filter<Aircraft>("Category: " + category,
+                                aircraft -> aircraft.getCategory() == category))
+                        .toList()
+        );
+    }
+
     public void install() {
         XPlane xPlane = xPlaneProperty.get();
         InstallWizard wizard = new InstallWizard(xPlane, InstallType.AIRCRAFT);
@@ -68,5 +91,10 @@ public class AircraftsController extends Controller {
 
     public void reload() {
         uiItems.reload();
+    }
+
+    public void applyFilter() {
+        Predicate<Aircraft> predicate = filterCombo.getValue().getPredicate();
+        filteredUiItems.setPredicate(uiAircraft -> predicate.test(uiAircraft.getAircraft()));
     }
 }
