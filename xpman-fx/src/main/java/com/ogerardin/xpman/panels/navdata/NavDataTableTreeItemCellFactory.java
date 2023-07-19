f5fefa03414672ea0a7266d8a4d7280668978caa
Olivier Gérardin <ogerardin@yahoo.com>
2023-07-19 17:15:13 +0200

    Nav data description window is now a singleton

--- xpman-fx/src/main/java/com/ogerardin/xpman/panels/navdata/NavDataTableTreeItemCellFactory.java
+++ xpman-fx/src/main/java/com/ogerardin/xpman/panels/navdata/NavDataTableTreeItemCellFactory.java
@@ -1,8 +1,8 @@
 package com.ogerardin.xpman.panels.navdata;
 
 import com.ogerardin.xpman.util.jfx.TreeTableCellFactory;
+import com.ogerardin.xpman.util.jfx.WebViewStage;
 import javafx.geometry.Insets;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TreeTableCell;
 import javafx.scene.control.TreeTableColumn;
@@ -10,22 +10,30 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.Border;
-import javafx.scene.web.WebEngine;
-import javafx.scene.web.WebView;
-import javafx.stage.Stage;
+import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 
+/** Specialiozed {@link TreeTableCellFactory} for {@link UiNavDataItem}.
+ * Items that have a description are displayed with an info icon that opens a popup with the description.
+ */
 @Slf4j
 public class NavDataTableTreeItemCellFactory<S> implements TreeTableCellFactory<S, UiNavDataItem> {
 
     private static final Image ICON_INFO =
             new Image(NavDataTableTreeItemCellFactory.class.getResourceAsStream("/img/dialog-information.png"));
 
+    @Getter(lazy = true)
+    private final WebViewStage descriptionStage = createDescriptionStage();
+
+    private WebViewStage createDescriptionStage() {
+        return new WebViewStage();
+    }
+
 
     @Override
     public TreeTableCell<S, UiNavDataItem> call(TreeTableColumn<S, UiNavDataItem> param) {
 
-        return new TreeTableCell<S, UiNavDataItem>() {
+        return new TreeTableCell<>() {
 
             private final Button button = new Button();
 
@@ -55,18 +63,17 @@ public class NavDataTableTreeItemCellFactory<S> implements TreeTableCellFactory<
                     return;
                 }
                 setGraphic(button);
-                button.setOnMouseClicked(event -> {
-                    // display the description (HTML) in a WebView
-                    WebView webView = new WebView();
-                    WebEngine engine = webView.getEngine();
-                    engine.loadContent(description);
-                    Stage stage = new Stage();
-                    stage.setTitle("NavData set description");
-                    stage.setScene(new Scene(webView));
-                    stage.show();
-                });
+                button.setOnMouseClicked(event -> displayDescription(value.getName(), description));
 
             }
         };
+    }
+
+    private void displayDescription(String name, String description) {
+        // display the description (HTML) in a WebView
+        WebViewStage stage = getDescriptionStage();
+        stage.setTitle(name);
+        stage.loadContent(description);
+        stage.show();
     }
 }
