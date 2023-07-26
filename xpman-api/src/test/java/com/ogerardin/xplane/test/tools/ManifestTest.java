a5dcd8ed21a53b54e1676edaa012208585b2803a
Olivier Gérardin <ogerardin@yahoo.com>
2023-07-27 01:44:02 +0200

    Reworked tools manifests so that multiple variants can be specified in a single file.

--- xpman-api/src/test/java/com/ogerardin/xplane/test/tools/ManifestTest.java
+++ xpman-api/src/test/java/com/ogerardin/xplane/test/tools/ManifestTest.java
@@ -8,17 +8,32 @@ import org.junit.jupiter.api.Test;
 import java.net.URISyntaxException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
+import java.util.Objects;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 
 @Slf4j
 class ManifestTest {
 
     @Test
-    @SuppressWarnings("ConstantConditions")
     public void testGsonDeserialize() throws URISyntaxException {
-        Path manifestFile = Paths.get(getClass().getResource("/tools/opensceneryx-installer.json").toURI());
+        Path manifestFile = Paths.get(Objects.requireNonNull(getClass().getResource("/test/tools/x-updater.json")).toURI());
 
         Manifest manifest = JsonManifestLoader.loadManifest(manifestFile);
 
         log.info("parsed: {}", manifest);
+    }
+
+    @Test
+    public void testUnfoldManifest() throws URISyntaxException {
+        Path manifestFile = Paths.get(Objects.requireNonNull(getClass().getResource("/test/tools/x-updater.json")).toURI());
+        Manifest manifest = JsonManifestLoader.loadManifest(manifestFile);
+
+        List<Manifest> unfoldedManifests = manifest.unfold();
+        unfoldedManifests.forEach(m -> log.info("unfolded: {}", m));
+
+        assertThat(unfoldedManifests.size(), is(3));
     }
 }
