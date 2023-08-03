db8d964449ce2b922d6b12355860af152bae7cbe
Olivier Gérardin <ogerardin@yahoo.com>
2023-08-04 01:02:33 +0200

    Preparation for installer rewrite

---
+++ xpman-api/src/main/java/com/ogerardin/xplane/install/InstallSource.java
@@ -0,0 +1,8 @@
+package com.ogerardin.xplane.install;
+
+import com.ogerardin.xplane.inspection.Inspectable;
+
+// TODO make InstallableZip implement this interface and move install logic to a ZipInstaller
+public interface InstallSource extends Inspectable {
+
+}
