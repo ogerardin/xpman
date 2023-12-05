1be1aaf7f23257c4a9a3f7868a33c302e6f354e8
Olivier Gérardin <ogerardin@yahoo.com>
2023-12-06 00:37:08 +0100

    Playing with Mac native menu

--- xpman-fx/src/main/java/com/ogerardin/xpman/util/jfx/JfxApp.java
+++ xpman-fx/src/main/java/com/ogerardin/xpman/util/jfx/JfxApp.java
@@ -1,10 +1,14 @@
 package com.ogerardin.xpman.util.jfx;
 
+import de.jangassen.MenuToolkit;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
+import javafx.scene.control.MenuBar;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
@@ -31,8 +35,29 @@ public abstract class JfxApp<C extends JfxAppPrefs> extends Application {
 
         log.debug("Setting up stage");
         setupStage(primaryStage);
+
+        //FIXME native mac menu exhibits some problems...
+//        if (Platforms.getCurrent() == Platforms.MAC) {
+//            setMacNativeMenu(primaryStage);
+//        }
+
         primaryStage.show();
         log.debug("Ready!");
+    }
+
+    /**
+     * If the primary stage's scene is a Pane and it has a MenuBar, move it to the Mac's native menu bar
+     */
+    private static void setMacNativeMenu(Stage primaryStage) {
+        MenuToolkit tk = MenuToolkit.toolkit();
+        if (primaryStage.getScene().getRoot() instanceof Pane root) {
+            for (Node child : root.getChildren()) {
+                if (child instanceof MenuBar menuBar) {
+                    tk.setMenuBar(root, menuBar);
+                    break;
+                }
+            }
+        }
     }
 
     protected abstract void setupStage(Stage primaryStage);
