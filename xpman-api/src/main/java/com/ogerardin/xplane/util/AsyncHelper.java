0f957b7947b39d7f526558c5b2ebe08632c68c87
Olivier Gérardin <ogerardin@yahoo.com>
2026-09-12 02:59:30 +0200

    refactor(util): use shared executor in AsyncHelper to avoid thread creation overhead

--- xpman-api/src/main/java/com/ogerardin/xplane/util/AsyncHelper.java
+++ xpman-api/src/main/java/com/ogerardin/xplane/util/AsyncHelper.java
@@ -2,12 +2,19 @@ package com.ogerardin.xplane.util;
 
 import lombok.experimental.UtilityClass;
 
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 @UtilityClass
 public class AsyncHelper {
 
+    private final ExecutorService EXECUTOR = Executors.newSingleThreadExecutor(r -> {
+        Thread t = new Thread(r, "xpman-async");
+        t.setDaemon(true);
+        return t;
+    });
+
     public void runAsync(Runnable task) {
-        Executors.newSingleThreadExecutor().submit(task);
+        EXECUTOR.submit(task);
     }
 }
